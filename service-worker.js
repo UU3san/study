48f91dfe0092a27618eb34aff6e1ed3bf5cf8a5e
UU3san <info@gakushuin.nasa.co.jp>
2026-09-21 17:40:30 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='study-cards-pwa-v13';
+const CACHE='study-cards-pwa-v14';
 const CORE=['./','./index.html','./subjects.json','./manifest.webmanifest','./icon-192.png','./icon-512.png','./maskable-512.png','./apple-touch-icon.png','./favicon-32.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
